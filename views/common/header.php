15767154ba9224625e228399f4c04ff28e3239d1
Melissa Benard <benardmeli@yahoo.fr>
2023-01-25 14:56:03 +0100

    display the link in color in the main menu when the page is active

--- views/common/header.php
+++ views/common/header.php
@@ -1,17 +1,35 @@
+<?php
+//print_r(basename($_SERVER['REDIRECT_URL']));
+$currentPage = basename($_SERVER['REDIRECT_URL']);
+//echo $currentPage;
+?>
+
 <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <a href="<?= URL ?>accueil" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
             <img src="<?= URL ?>public/assets/images/logo-menu.png" alt="logo de Charles Cantin" width="150" class="rounded">
         </a>
 
-        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="<?= URL ?>galerie" class="nav-link px-2 link-dark">Galerie</a></li>
-            <li><a href="<?= URL ?>tarifs" class="nav-link px-2 link-dark">Tarifs</a></li>
-            <li><a href="<?= URL ?>contact" class="nav-link px-2 link-dark">Contact</a></li>
+        <ul class="nav nav-navbar col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li class="nav-item">
+                <a href="<?= URL ?>galerie" class="nav-link link-dark px-2 <?php if($currentPage === "galerie") {echo "active";} ?>">
+                    Galerie
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="<?= URL ?>tarifs" class="nav-link link-dark px-2 <?php if($currentPage === "tarifs") {echo "active";} ?>"">
+                    Tarifs
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="<?= URL ?>contact" class="nav-link link-dark px-2 <?php if($currentPage === "contact") {echo "active";} ?>"">
+                    Contact
+                </a>
+            </li>
         </ul>
 
         <div class="col-md-3 text-end">
-            <a href="connexion" class="btn btn-outline-dark me-2">
+            <a href="connexion" class="btn btn-outline-dark me-2 <?php if($currentPage === "connexion") {echo "active";} ?>"">
                 <i class="fa-solid fa-user"></i>
             </a>
         </div>
